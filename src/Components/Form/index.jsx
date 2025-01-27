9171064ac508e30e9388e800690dea8470e4924c
Pranali <pgmodgekar99@gmail.com>
2025-01-27 12:30:16 +0530

    validate form fields before submission and improve alert messages

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -95,7 +95,7 @@ function FormComp() {
                             !(selectedDate > new Date()) &&(
 
                             
-                            <section className=' shadow-lg  max-md:mt-[-30px] max-md:w-[105%]'>
+                            <section className=' shadow-lg  max-md:mt-[-30px] max-md:w-[107%]'>
                                 <h3 className="text-xl font-bold mb-4  ml-5 mt-5 ">{t('formSection.chooseDate')}</h3>
                                 <div className="bg-white shadow-lg rounded-lg p-2">
                                     <Calendar
