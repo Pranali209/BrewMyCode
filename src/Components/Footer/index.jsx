39db41746b4957728c9560f04f5bb13b04d57ca8
Pranali <pgmodgekar99@gmail.com>
2025-04-02 12:40:08 +0530

    Googlesheet added for email

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,9 +3,29 @@ import Heading from '../Heading';
 import Button from '../Button';
 
 import { useTranslation } from 'react-i18next';
-
+//https://script.google.com/macros/s/AKfycbwHfh010JV4oXiouRE0AHgoBh1kbmRPVL64g-gBDzpwYGqAg3nZV9zLUvejW9KJ7kTU8g/exec
 export default function Footer({handleBookCall}) {
   const { t } = useTranslation();
+  const [res , setRes] = React.useState('')
+  const handleSubcribe = (e) => {
+        e.preventDefault();
+        console.log(e.target[0].value);
+        
+        const url = "https://script.google.com/macros/s/AKfycbwHfh010JV4oXiouRE0AHgoBh1kbmRPVL64g-gBDzpwYGqAg3nZV9zLUvejW9KJ7kTU8g/exec"
+        fetch(url,{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body:(
+            `email= ${e.target[0].value}`
+          )
+        }).then((res) => res.text()).then((data)=>{
+           setRes(data)
+        }).catch((err) => {
+          console.log(err)
+        })
+  }
   return (
     <footer className="container p-8 mx-auto text-white bg-black px-14 py-28 max-md:px-8 ">
         <Heading  className="mb-4 text-5xl font-semibold text-white" as =" h1" text = {t('footerHeading')} />
@@ -17,7 +37,7 @@ export default function Footer({handleBookCall}) {
         <li>{t('footersubheading2')}</li>
       </ul>
       <p className="mb-4 text-lg font-semibold">{t('footersubheading3')}</p>
-      <div className="flex justify-start">
+      <form className="flex justify-start" onSubmit={handleSubcribe}>
         <input
           type="email"
             className="text-black max-md:w-[90%] w-[30%] px-2 py-2 mr-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -25,8 +45,10 @@ export default function Footer({handleBookCall}) {
         />
       
           <button className=' bg-[#D2F2FF]  text-black px-3 py-1 max-md:px-1  max-md:text-sm  rounded-full'
-         >{t('footerbtn')}</button>
-      </div>
+      >{t('footerbtn')}</button>
+     
+      </form>
+      {res && <p className='mt-5 text-green-500'>{res}</p>}
      <div className='h-20'></div>
     </footer>
   );
